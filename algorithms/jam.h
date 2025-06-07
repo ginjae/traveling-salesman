7181b37c7b855ab71366b0e7efc1cdd06fc0a792
ginjae <jm611@unist.ac.kr>
2025-06-08 01:00:46 +0900

    Add result with k=n

--- algorithms/jam.h
+++ algorithms/jam.h
@@ -4,14 +4,13 @@
 #include "tsp_solver.h"
 #include "greedy_edge.h"
 
-#include <cmath>
 
 class Jam : public TSPSolver {
 public:
     GreedyEdge ge;
     void solve(const std::vector<Node>& nodes) override {
         int n = nodes.size();
-        this->solve_with_topk(nodes, nodes.size() * std::log(nodes.size()));
+        this->solve_with_topk(nodes, nodes.size());
         // if (n < 10'000) {
         //     ge.solve(nodes);
         //     this->solution = ge.solution;

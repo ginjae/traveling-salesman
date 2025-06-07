531a221787dab38c9a829f3dc8f097f94193d2cd
ginjae <jm611@unist.ac.kr>
2025-06-08 01:04:58 +0900

    Revert "Add result with k=n"
    This reverts commit 7181b37c7b855ab71366b0e7efc1cdd06fc0a792.

--- algorithms/jam.h
+++ algorithms/jam.h
@@ -4,13 +4,14 @@
 #include "tsp_solver.h"
 #include "greedy_edge.h"
 
+#include <cmath>
 
 class Jam : public TSPSolver {
 public:
     GreedyEdge ge;
     void solve(const std::vector<Node>& nodes) override {
         int n = nodes.size();
-        this->solve_with_topk(nodes, nodes.size());
+        this->solve_with_topk(nodes, nodes.size() * std::log(nodes.size()));
         // if (n < 10'000) {
         //     ge.solve(nodes);
         //     this->solution = ge.solution;
